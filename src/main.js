1671924aa1245e628ce82c37c167ebf94c9df21d
Jackz <me@jackz.me>
2019-12-03 21:40:00 -0600

    initial

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowCircleRight, faTimesCircle} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faArrowCircleRight);
+library.add(faTimesCircle);
+
+Vue.use(VueRouter)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(Buefy, { defaultIconPack: 'fas' })
 
 Vue.config.productionTip = false
 
