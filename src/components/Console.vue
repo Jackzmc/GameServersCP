db961016a101368c2884f66a785d24ebeaa61879
Jackz <me@jackz.me>
2019-12-06 14:57:08 -0600

    Added console UI

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -1,12 +1,13 @@
 <template>
 <div id="app">
-<h6 class="title is-6">Console</h6>
-<article class="message is-warning">
-    <div class="message-body">
-        <strong>Notice</strong>
-        This component is not implemented.
-    </div>
-</article>
+    <h6 class="title is-6">Console</h6>
+    <b-field>
+        <b-input type="textarea" :loading="loading" readonly v-model="console" expanded custom-class="console"></b-input>
+    </b-field>
+    <b-field>
+        <b-input :disabled="loading" placeholder="Enter a command here" icon="caret-right" v-model="input" expanded />
+    </b-field>
+    <span v-if="loading">Waiting for server...</span>
 </div>
 </template>
 
@@ -14,13 +15,24 @@
 export default {
     data() {
         return {
-
+            loading: true,
+            console:``,
+            input:null
         }
     },
     props:{
         server:{}
     },
     mounted() {
+        
     }
 }
 </script>
+
+<style>
+.console {
+    background-color: black;
+    color: white;
+    height: 500px;
+}
+</style>
