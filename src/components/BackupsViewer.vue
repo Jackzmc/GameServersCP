a7c99f9daa7efa18b28fcca9e5c5e4b90e238968
Jackz <me@jackz.me>
2019-12-07 11:29:02 -0600

    added backups list & btns

--- src/components/BackupsViewer.vue
+++ src/components/BackupsViewer.vue
@@ -4,8 +4,56 @@
 <article class="message is-warning">
   <div class="message-body">
    <strong>Notice</strong>
-   This component is not implemented.
+   This component is not fully implemented.
   </div>
 </article>
+<b-table :data="backups">
+  <template slot-scope="props" >
+    <b-table-column label="Name">
+      {{props.row.name}}
+    </b-table-column>
+    <b-table-column field="created" label="Created" class="has-text-middle" width="180">
+        {{props.row.created | formatDate}}
+    </b-table-column>
+    <b-table-column field="size" label="File Size" class="has-text-middle" width="100">
+        {{props.row.size | humanizeSize}}
+    </b-table-column>
+    <b-table-column  label="Action" class="has-text-middle" width="350">
+      <div class="buttons">
+        <b-button icon-left="download">Download</b-button>
+        <b-button icon-left="search">Inspect</b-button>
+        <b-button icon-left="trash" type="is-danger">Delete</b-button>
+      </div>
+    </b-table-column>
+  </template>
+</b-table>
 </div>
 </template>
+
+<script>
+import Axios from 'axios';
+export default {
+  data() {
+    return {
+      backups:[]
+    }
+  },
+  props:['server'],
+  mounted() {
+    Axios.get(`${this.$apiURL}/server/${this.server._id}/backups`,{json:true}).then((r) => {
+        this.backups = r.data;
+    }).catch(err => {
+        this.$buefy.dialog.alert({
+            title: 'Error',
+            message: `<b>Something happened while fetching backups.</b><br>${err.message} `,
+            type: 'is-danger',
+            hasIcon: true,
+            icon: 'times-circle',
+            iconPack: 'fa',
+            ariaRole: 'alertdialog',
+            ariaModal: true
+        })
+    })
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ import moment from 'moment';
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faArrowCircleRight, faTimesCircle, faLongArrowAltLeft, faCogs, faList, faFileArchive, faSave, faArrowUp, faTerminal, faInfo, faCaretRight, faPencilAlt, faAngleUp, faAngleDown, faTag} from '@fortawesome/free-solid-svg-icons'
+import { faDownload, faSearch, faTrash, faArrowCircleRight, faTimesCircle, faLongArrowAltLeft, faCogs, faList, faFileArchive, faSave, faArrowUp, faTerminal, faInfo, faCaretRight, faPencilAlt, faAngleUp, faAngleDown, faTag} from '@fortawesome/free-solid-svg-icons'
 import router from './router'
 
-library.add(faArrowCircleRight, faTimesCircle, faLongArrowAltLeft, faCogs, faList, faFileArchive, faSave, faArrowUp, faTerminal, faInfo, faCaretRight, faPencilAlt, faAngleUp, faAngleDown, faTag);
+library.add(faDownload, faSearch, faTrash, faArrowCircleRight, faTimesCircle, faLongArrowAltLeft, faCogs, faList, faFileArchive, faSave, faArrowUp, faTerminal, faInfo, faCaretRight, faPencilAlt, faAngleUp, faAngleDown, faTag);
 
 Vue.use(VueRouter)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
@@ -21,12 +21,18 @@ Vue.prototype.$apiURL = process.env.VUE_APP_API_URL
 
 Vue.filter('formatDate', function(value) {
   if (value) {
-    return moment(String(value)).format('MM/DD/YYYY hh:mm')
+    return moment(String(value)).format('YYYY/MM/DD [at] hh:mm')
   }
 })
 Vue.filter('formatUptime', function(value) {
   if (value) {
     return moment(value).fromNow(true);
+  }
+})
+Vue.filter('humanizeSize', function(size) {
+  if(size) {
+    var i = Math.floor( Math.log(size) / Math.log(1024) );
+    return ( size / Math.pow(1024, i) ).toFixed(1) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
   }
 })
 
