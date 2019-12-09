004e4b0408ec3e5386e3d2ffd57daa126a869f6c
Jackz <me@jackz.me>
2019-12-08 20:14:46 -0600

    Worked more on create page

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -2,11 +2,40 @@
 <div id="app" class="container">
     <h4 class="title is-4 has-text-centered">Create New Server</h4>
     <form>
-        <b-field label="Enter ID" message="Must be a unique, single alphanumeric combination">
-            <b-input v-model="id"/>
+        <b-field label="Enter ID (optional)" message="Must be a unique, single alphanumeric combination. Leave blank for a random UUID">
+            <b-input v-model="id" placeholder="csgo" />
         </b-field>
         <b-field label="Enter Display Title" message="">
-            <b-input v-model="title"/>
+            <b-input v-model="title" placeholder="My CSGO Server" required />
+        </b-field>
+        <b-field label="Game Type">
+            <b-field>
+                <b-radio-button v-model="type" native-value="minecraft">
+                    <span>Minecraft</span>
+                </b-radio-button>
+
+                <b-radio-button v-model="type" native-value="sourcegame">
+                    <span>Source Game</span>
+                </b-radio-button>
+            </b-field>
+        </b-field>
+        <b-field v-if="type == 'minecraft'" label="Server Type">
+            <b-field >
+                <b-radio-button v-for="(name,index) in $options.JARS" :key="name" v-model="jar" :native-value="index">
+                    <span class="is-capitalized">{{name}}</span>
+                </b-radio-button>
+            </b-field>
+        </b-field>
+        <div v-if="type == 'sourcegame'">
+            <b-field  label="Server AppID" message="Make sure the appID is a dedicated server, not the game client.">
+                <b-input type="number" v-model="appid" placeholder="222860" />
+            </b-field>
+            <a href="https://developer.valvesoftware.com/wiki/Dedicated_Servers_List">Click here to view official list of dedicated server appids</a>
+            <br>
+        </div>
+        <br>
+        <b-field>
+            <b-button @click="createServer" :disabled="isDisabled" type="is-primary" size="is-large">Create Server</b-button>
         </b-field>
     </form>
 
@@ -15,10 +44,33 @@
 
 <script>
 export default {
+    JARS: ['vanilla','spigot','sponge','paper'],
     data() {
         return {
             title:null,
-            type:null
+            type:'',
+            jar:0,
+            appid:null
+        }
+    },
+    computed:{
+        isDisabled() {
+            if(this.type) {
+                if(this.type == "sourcegame") {
+                    return !(this.appid)
+                }
+                return false;
+            }else{
+                return true;
+            }
+        }
+    },
+    methods:{
+        createServer() {
+            this.$buefy.toast.open({
+                message:'Not implemented',
+                type:'is-danger'
+            })
         }
     },
     props:['id']
